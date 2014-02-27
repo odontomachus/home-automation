849b8c88151833e4343abce591664c34e867edc3
Jonathan Villemaire-Krajden <odontomachus@gmail.com>
2014-02-27 11:22:23 -0500

    Fixes to forking and pid locking.

--- remotes/desktop/server.py
+++ remotes/desktop/server.py
@@ -32,6 +32,24 @@ if __name__=="__main__":
         os.umask(0)
         pid2 = os.fork()
         if (pid2 == 0):  # Second child
+            try:
+                pidfile = os.environ['PIDFile']
+            except KeyError as e:
+                pidfile = '/var/run/lightswitch/lightswitch.pid'
+            try:
+                pidfd = os.open(pidfile,
+                                os.O_CREAT | # create file
+                                os.O_TRUNC | # truncate it, if it exists
+                                os.O_WRONLY | # write-only
+                                os.O_EXCL, # exclusive access
+                                0644) # file mode
+
+            except Exception, e:
+                print >> sys.stderr, 'Could not create pidfile {pidfile}'.format(pidfile=pidfile)
+                sys.exit(1)
+
+            os.write(pidfd, "%s\n" % os.getpid())
+            os.fsync(pidfd)
             run(host='0.0.0.0', port=8090)
         else:
             sys.exit()    #First child exits for decoupling
